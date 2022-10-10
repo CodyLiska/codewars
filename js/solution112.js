e4143a4ae68e90aac62a19f95575817760a3c7b1
Cody Liska <codyliska@gmail.com>
2022-10-10 16:16:06 -0700

    solution112.js completed

---
+++ js/solution112.js
@@ -0,0 +1,18 @@
+/*
+Create a function, helloWorld.
+Use let, const, or int to set str equal to "Hello World!"
+Console log str
+*/
+
+function helloWorld(){
+  let str = "Hello World!"
+  console.log(str)
+}
+
+/*
+Now you have to write a function that takes an argument and returns the square of it.
+*/
+
+function square(n){
+  return Math.pow(n, 2)
+}
