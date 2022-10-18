bf59e5bb4e2e46064fec57683860ad637638b0cf
Cody Liska <codyliska@gmail.com>
2022-10-18 16:05:38 -0700

    solution117 completed

---
+++ js/solution117.js
@@ -0,0 +1,14 @@
+/*
+Complete the function which converts hex number (given as a string) to a decimal number.
+*/
+
+function hexToDec(hexString){
+  //your code here
+  return parseInt(hexString, 16)
+}
+
+/* Converted to ARROW FUNCTION 
+
+  const hexToDec = hex => parseInt(hex, 16)
+
+*/
